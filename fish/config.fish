ac264b1059790939f2bb76ea14f86876287d107b
Finlifin <c2183315367@outlook.com>
2022-11-15 17:19:52 +0800

    config setup

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    alias s "startplasma-wayland"
+    alias sb "sudo helix /sys/class/backlight/amdgpu_bl0/brightness"
+    alias :q "exit"
+    # Commands to run in interactive sessions can go here
+end
