acaa6e2ad9fd55be4a9bee45bfa41be093d30e7c
Finlifin <c2183315367@outlook.com>
2022-12-16 17:31:01 +0800

    released config on 2022.12.16

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,12 +3,14 @@ function fish_prompt
         echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
 
-    echo -n (set_color blue)(prompt_pwd)' '
-
     set_color -o
+    echo -n (set_color white)'/*--------------*\
+    '
+
     if fish_is_root_user
         echo -n (set_color red)'# '
     end
-    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    echo (set_color white)(prompt_pwd)
+    echo -n '\*--------------*/'' ' (set_color white)'|->'
     set_color normal
 end
